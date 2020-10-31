d0a6c412ce9b59990c2edcce60ebc14fd151e453
阮传宙 <ruanchuanzhou@interlib.vip>
2020-10-31 15:28:11 +0800

    fix: 完成图像验证码接口

--- src/api/PublicController.js
+++ src/api/PublicController.js
@@ -1,12 +1,21 @@
 import svgCaptcha from 'svg-captcha'
 
 class PublicController {
-  constructor() {}
-  async getCaptcha(ctx) {
-    const newCaptcha = new svgCaptcha.create({})
+  constructor () {
+  }
+
+  async getCaptcha (ctx) {
+    const newCaptcha = new svgCaptcha.create({
+      size: 4, // 数量
+      ignoreChars: '0oil', // 排除某些字符
+      color: true, // 使用颜色
+      noise: Math.floor((Math.random() * 5)),  // 线条的数量
+      width: 150, // 宽度
+      height: 50 // 高度
+    })
     ctx.body = {
-        code:200,
-        data:newCaptcha.data
+      code: 200,
+      data: newCaptcha.data
     }
   }
 }
